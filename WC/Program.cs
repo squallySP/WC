9d997126695e72219f9cd6f6573b53a921175e70
squallySP <lihaofu@gmail.com>
2015-03-27 13:44:35 +0800

    new

---
+++ WC/Program.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace WC
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] fileDir = new string[2] {null, null};
+            bool charCount = false;
+            bool wordCount = false;
+            bool lineCount = false;
+            bool subFolder = false;
+            bool advance = false;
+            bool window = false;
+            int fileDirNum = 0;
+
+            #region 解析命令参数
+            foreach (string word in args)
+            {
+                if (word.Substring(0, 1) == "-")
+                {
+                    if (word == "-c") charCount = true;
+                    else if (word == "-w") wordCount = true;
+                    else if (word == "-l") lineCount = true;
+                    else if (word == "-s") subFolder = true;
+                    else if (word == "-a") advance = true;
+                    else if (word == "-x") window = true;
+                    else
+                    {
+                        Console.WriteLine("参数错误： " + word);
+                        return;
+                    }
+                }
+                else
+                {
+                    if (fileDirNum < 2)
+                    {
+                        fileDir[fileDirNum] = word;
+                        fileDirNum += 1;
+                    }
+                    else
+                    {
+                        Console.WriteLine("文件名或路径错误： " + fileDir + ", " + word);
+                        return;
+                    }
+
+                }
+            } 
+            #endregion
+
+            #region check the fileDir is valid
+            //try
+            //{
+            //    StreamReader sr = new StreamReader(fileDir);
+            //}
+            //catch
+            //{
+            //    Console.WriteLine("filename or directory error! ");
+            //    return;
+            //} 
+            #endregion
+
+            #region 执行操作
+            string fileName = fileDir[0] + fileDir[1];
+            if (charCount) Console.WriteLine("charCount: " + CharCount(fileName));
+            if (wordCount) Console.WriteLine("wordCount: " + WordCount(fileName));
+            if (lineCount) Console.WriteLine("lineCount: " + LineCount(fileName)); 
+            #endregion
+
+            Console.Read();
+        }
+
+        public static int CharCount(string fileName)
+        {
+            //
+            int result = 0;
+            try
+            {
+                StreamReader sr_try = new StreamReader(fileName);
+                sr_try.Close();
+            }
+            catch
+            {
+                throw new ArgumentException();
+            }
+            StreamReader sr = new StreamReader(fileName);
+            string line = sr.ReadLine();
+            while(line != null){
+                foreach (char c in line)
+                {
+                    if (c == ' ' || c == '\t')
+                        result += 0;
+                    else
+                        result += 1;
+                }
+                line = sr.ReadLine();
+            }
+            sr.Close();
+            return result;
+        }
+
+        public static int WordCount(string fileName)
+        {
+            
+            //
+            int result = 0;
+            try
+            {
+                StreamReader sr_try = new StreamReader(fileName);
+                sr_try.Close();
+            }
+            catch
+            {
+                throw new ArgumentException();
+            }
+            StreamReader sr = new StreamReader(fileName);
+            string line = sr.ReadLine();
+            while (line != null)
+            {
+                bool inWord = false;
+                foreach (char c in line)
+                {
+                    if (c == ' ' || c == '\t')
+                        inWord = false;
+                    else if (!inWord)
+                    {
+                        result += 1;
+                        inWord = true;
+                    }
+                        
+                }
+                line = sr.ReadLine();
+            }
+            sr.Close();
+            return result;
+        }
+
+        public static int LineCount(string fileName)
+        {
+            //
+            int result = 0;
+            try
+            {
+                StreamReader sr_try = new StreamReader(fileName);
+                sr_try.Close();
+            }
+            catch
+            {
+                throw new ArgumentException();
+            }
+            StreamReader sr = new StreamReader(fileName);
+            string line = sr.ReadLine();
+            while (line != null)
+            {
+                result += 1;
+                line = sr.ReadLine();
+            }
+            sr.Close();
+            return result;
+        }
+
+        public static int[] LineCountAdvance(string fileName)
+        {
+            // int[0]空行数  int[1]注释行数  int[2]代码行数量
+            int[] result = {0,0,0};
+            try
+            {
+                StreamReader sr_try = new StreamReader(fileName);
+                sr_try.Close();
+            }
+            catch
+            {
+                throw new ArgumentException();
+            }
+            StreamReader sr = new StreamReader(fileName);
+            string line = sr.ReadLine();
+            bool InCommentBlock = false;
+            while (line != null)
+            {
+                bool isComment = false;
+                bool isCode = false;
+                if (!InCommentBlock)
+                {
+                    //int codeEnd = line.Length;
+                    string code = line;
+
+                    int indexOfFF = code.IndexOf("//");
+                    if (indexOfFF > -1)
+                    {
+                        isComment = true;
+                        code = code.Substring(0, indexOfFF);
+                    }
+                    int indexOfFS = code.IndexOf("/*");
+                    if (indexOfFS > -1)
+                    {
+                        isComment = true;
+                        InCommentBlock = true;
+                        code = code.Substring(0, indexOfFS);
+
+                        string comment = line.Substring(indexOfFS+2);
+                        if (comment.IndexOf("*/") > -1)
+                            InCommentBlock = false;
+                    }
+                    isCode = LineHasCode(code);
+                }
+                else
+                {
+                    isComment = true;
+                    if (line.Length == 0)
+                        isComment = false;
+                    else if (line.IndexOf("*/")>-1)
+                    {
+                        InCommentBlock = false;
+                    }
+                }
+                
+                
+                if (isCode)
+                    result[2] += 1;
+                else if (isComment)
+                    result[1] += 1;
+                else
+                    result[0] += 1;
+                
+                line = sr.ReadLine();
+            }
+            sr.Close();
+            return result;
+        }
+
+        public static bool LineHasCode(string code)
+        {
+            int codeCharNum = 0;
+            foreach(Char c in code){
+                if(c!=' '&&c!='\t')
+                    codeCharNum += 1;
+            }
+            if (codeCharNum > 1)
+                return true;
+            else
+                return false;
+        }
+
+        public static List<DirectoryInfo> GetSubFolders(string dir)
+        {
+            
+            if (!Directory.Exists(dir))
+                throw new ArgumentException();
+
+            List<DirectoryInfo> result = new List<DirectoryInfo>();
+            DirectoryInfo dirInfo = new DirectoryInfo(dir);
+            result.Add(dirInfo);
+            result = GetSubDirs(dirInfo, result);
+            
+            return result;
+        }
+        public static List<DirectoryInfo> GetSubDirs(DirectoryInfo dirInfo, List<DirectoryInfo> result)
+        {
+            foreach (DirectoryInfo subDirInfo in dirInfo.GetDirectories())
+            {
+                result.Add(subDirInfo);
+                result = GetSubDirs(subDirInfo, result);
+            }
+            return result;
+        }
+        
+        // 获取制定路径下的文件，支持文件通配符
+        public static List<FileInfo> GetFiles(List<DirectoryInfo> dirInfos , string fileName)
+        {
+            List<FileInfo> result = new  List<FileInfo>();
+            foreach(DirectoryInfo dirInfo in dirInfos){
+                foreach (FileInfo fileInfo in dirInfo.GetFiles(fileName))
+                {
+                    result.Add(fileInfo);
+                }
+            }
+            return result;
+        }
+
+    }
+}
---
+++ WcTest/ProgramTest.cs
@@ -0,0 +1,328 @@
+﻿using System;
+using System.IO;
+using WC;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WcTest
+{
+    [TestClass]
+    public class ProgramTest
+    {
+
+        #region CharCount Tests
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CharCountTest1()
+        {
+            Program.CharCount(" ");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CharCountTest1_1()
+        {
+            Program.CharCount(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CharCountTest1_2()
+        {
+            Program.CharCount("__");
+        }
+
+        [TestMethod]
+        public void CharCountTest2()
+        {
+            Assert.AreEqual(0, Program.CharCount("/WcTestFiles/0_empty.txt"));
+        }
+
+        [TestMethod]
+        public void CharCountTest3()
+        {
+            Assert.AreEqual(0, Program.CharCount("/WcTestFiles/0_space.txt"));
+        }
+
+        [TestMethod]
+        public void CharCountTest4()
+        {
+            Assert.AreEqual(0, Program.CharCount("/WcTestFiles/0_tab.txt"));
+        }
+
+        [TestMethod]
+        public void CharCountTest5()
+        {
+            Assert.AreEqual(1, Program.CharCount("/WcTestFiles/1_char1.txt"));
+        }
+
+        [TestMethod]
+        public void CharCountTest6()
+        {
+            Assert.AreEqual(1, Program.CharCount("/WcTestFiles/1_char2.txt"));
+        }
+
+        [TestMethod]
+        public void CharCountTest7()
+        {
+            Assert.AreEqual(5, Program.CharCount("/WcTestFiles/5_char.txt"));
+        } 
+        #endregion
+
+        #region WordCount Tests
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void WordCountTest1()
+        {
+            Program.WordCount(" ");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void WordCountTest1_1()
+        {
+            Program.WordCount(null);
+        }
+
+        [TestMethod]
+        public void WordCountTest2()
+        {
+            Assert.AreEqual(0, Program.WordCount("/WcTestFiles/0_empty.txt"));
+        }
+
+        [TestMethod]
+        public void WordCountTest3()
+        {
+            Assert.AreEqual(0, Program.WordCount("/WcTestFiles/0_space.txt"));
+        }
+
+        [TestMethod]
+        public void WordCountTest4()
+        {
+            Assert.AreEqual(0, Program.WordCount("/WcTestFiles/0_tab.txt"));
+        }
+
+        [TestMethod]
+        public void WordCountTest5()
+        {
+            Assert.AreEqual(1, Program.WordCount("/WcTestFiles/1_char1.txt"));
+        }
+
+        [TestMethod]
+        public void WordCountTest6()
+        {
+            Assert.AreEqual(1, Program.WordCount("/WcTestFiles/1_char2.txt"));
+        }
+
+        [TestMethod]
+        public void WordCountTest7()
+        {
+            Assert.AreEqual(4, Program.WordCount("/WcTestFiles/5_char.txt"));
+        }
+
+        [TestMethod]
+        public void WordCountTest8()
+        {
+            Assert.AreEqual(5, Program.WordCount("/WcTestFiles/5_word_5.txt"));
+        } 
+        #endregion
+
+        #region LineCount Tests
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void LineCountTest1()
+        {
+            Program.LineCount(" ");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void LineCountTest1_1()
+        {
+            Program.LineCount(null);
+        }
+
+        [TestMethod]
+        public void LineCountTest2()
+        {
+            Assert.AreEqual(0, Program.LineCount("/WcTestFiles/0_empty.txt"));
+        }
+
+        [TestMethod]
+        public void LineCountTest3()
+        {
+            Assert.AreEqual(1, Program.LineCount("/WcTestFiles/0_space.txt"));
+        }
+
+        [TestMethod]
+        public void LineCountTest4()
+        {
+            Assert.AreEqual(1, Program.LineCount("/WcTestFiles/0_tab.txt"));
+        }
+
+        [TestMethod]
+        public void LineCountTest5()
+        {
+            Assert.AreEqual(1, Program.LineCount("/WcTestFiles/1_char1.txt"));
+        }
+
+        [TestMethod]
+        public void LineCountTest6()
+        {
+            Assert.AreEqual(1, Program.LineCount("/WcTestFiles/1_char2.txt"));
+        }
+
+        [TestMethod]
+        public void LineCountTest7()
+        {
+            Assert.AreEqual(5, Program.LineCount("/WcTestFiles/5_char.txt"));
+        }
+
+        [TestMethod]
+        public void LineCountTest8()
+        {
+            Assert.AreEqual(6, Program.LineCount("/WcTestFiles/5_word_5.txt"));
+        }
+
+        [TestMethod]
+        public void LineCountTest9()
+        {
+            Assert.AreEqual(6, Program.LineCount("/WcTestFiles/6_line_6.txt"));
+        }
+        #endregion
+
+        #region GetSubFolders Tests
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetSubFoldersTest1()
+        {
+            Program.GetSubFolders(" ");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetSubFoldersTest1_1()
+        {
+            Program.GetSubFolders(null);
+        }
+
+        [TestMethod]
+        public void GetSubFoldersTest2()
+        {
+            Assert.AreEqual(3, Program.GetSubFolders("/WcTestFiles/").Count);
+        }
+
+        [TestMethod]
+        public void GetSubFoldersTest3()
+        {
+            Assert.AreEqual(2, Program.GetSubFolders("/WcTestFiles/subFolder/").Count);
+        }
+
+        [TestMethod]
+        public void GetSubFoldersTest4()
+        {
+            Assert.AreEqual(1, Program.GetSubFolders("/WcTestFiles/subFolder/ssFolder").Count);
+        }
+        #endregion
+
+        #region GetFiles Tests
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void GetFilesTest1_1()
+        {
+            Program.GetFiles(null, "*");
+        }
+
+        [TestMethod]
+        public void GetFilesTest2()
+        {
+            List<DirectoryInfo> dirInfos = new List<DirectoryInfo>();
+            dirInfos.Add(new DirectoryInfo("/WcTestFiles/"));
+            dirInfos.Add(new DirectoryInfo("/WcTestFiles/subFolder/"));
+            dirInfos.Add(new DirectoryInfo("/WcTestFiles/subFolder/ssFolder"));
+            Assert.AreEqual(17, Program.GetFiles(dirInfos, "*").Count);
+        }
+
+        [TestMethod]
+        public void GetFilesTest3()
+        {
+            List<DirectoryInfo> dirInfos = new List<DirectoryInfo>();
+            dirInfos.Add(new DirectoryInfo("/WcTestFiles/subFolder/"));
+            dirInfos.Add(new DirectoryInfo("/WcTestFiles/subFolder/ssFolder"));
+            Assert.AreEqual(6, Program.GetFiles(dirInfos, "*").Count);
+        }
+
+        [TestMethod]
+        public void GetFilesTest4()
+        {
+            List<DirectoryInfo> dirInfos = new List<DirectoryInfo>();
+            dirInfos.Add(new DirectoryInfo("/WcTestFiles/subFolder/ssFolder"));
+            Assert.AreEqual(3, Program.GetFiles(dirInfos, "*").Count);
+        }
+
+        [TestMethod]
+        public void GetFilesTest5()
+        {
+            List<DirectoryInfo> dirInfos = new List<DirectoryInfo>();
+            dirInfos.Add(new DirectoryInfo("/WcTestFiles/subFolder/"));
+            dirInfos.Add(new DirectoryInfo("/WcTestFiles/subFolder/ssFolder"));
+            Assert.AreEqual(3, Program.GetFiles(dirInfos, "sub?.txt").Count);
+        }
+
+        [TestMethod]
+        public void GetFilesTest6()
+        {
+            List<DirectoryInfo> dirInfos = new List<DirectoryInfo>();
+            dirInfos.Add(new DirectoryInfo("/WcTestFiles/subFolder/"));
+            dirInfos.Add(new DirectoryInfo("/WcTestFiles/subFolder/ssFolder"));
+            Assert.AreEqual(3, Program.GetFiles(dirInfos, "ss?.txt").Count);
+        }
+        #endregion
+
+        #region LineCountAdvance Tests
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void LineCountAdvanceTest1()
+        {
+            Program.LineCountAdvance(" ");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void LineCountAdvanceTest1_1()
+        {
+            Program.LineCountAdvance(null);
+        }
+
+        [TestMethod]
+        public void LineCountAdvanceTest2()
+        {
+            var result = Program.LineCountAdvance("/WcTestFiles/6_line_adv1_2_1_3.txt");
+            Assert.AreEqual(2, result[0]);
+            Assert.AreEqual(1, result[1]);
+            Assert.AreEqual(3, result[2]);
+        }
+
+        [TestMethod]
+        public void LineCountAdvanceTest3()
+        {
+
+            var result = Program.LineCountAdvance("/WcTestFiles/6_line_adv2_3_3_3.txt");
+            Assert.AreEqual(3, result[0]);
+            Assert.AreEqual(3, result[1]);
+            Assert.AreEqual(3, result[2]);
+        }
+
+        [TestMethod]
+        public void LineCountAdvanceTest4()
+        {
+
+            var result = Program.LineCountAdvance("/WcTestFiles/6_line_adv3_5_8_4.txt");
+            Assert.AreEqual(5, result[0]);
+            Assert.AreEqual(8, result[1]);
+            Assert.AreEqual(4, result[2]);
+        }
+        #endregion
+    }
+}
